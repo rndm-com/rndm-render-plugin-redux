79d7212b21635a860fa884004ae069cb82dd6d4f
Paul Napier <>
2019-01-08 17:52:34 +1100

    Apply functionChain rendering through dispatch objects

--- src/middleware/connect/buildDispatch.js
+++ src/middleware/connect/buildDispatch.js
@@ -1,9 +1,15 @@
-import { set } from 'lodash';
+import { set,  } from 'lodash';
+import { render } from '@rndm/render';
 
 const buildDispatch = (input = []) => (
   input.reduce((o, { prop, action }) => {
     const output = {...o};
-    set(output, prop, () => action);
+    set(output, render(prop), (...args) => {
+      return Object.keys(action).reduce((o,i) => {
+        const value = render(action[i], 'RNDM.functionChain', { args });
+        return ({ ...o, [i]: value })
+      }, {});
+    });
     return output;
   }, {})
 );
