71ed97ac26ca14b04a3f411f466e86afcd9f1de7
Paul Napier <paul.j.napier@gmail.com>
2019-01-28 17:00:44 +1100

    Allow RNDM to render down to, from and default value for state building

--- src/middleware/connect/buildState.js
+++ src/middleware/connect/buildState.js
@@ -1,12 +1,16 @@
+import { render } from '@rndm/render';
 import { get, set } from 'lodash';
 
 const buildState = (input = []) => (state) => (
-  !Array.isArray(input) ? null :
-  input.reduce((o, { from, to, default: value = null } = {}) => {
-    const output = {...o};
-    set(output, to, get(state, from, value));
-    return output;
-  }, {})
+    !Array.isArray(input) ? null :
+        input.reduce((o, { from, to, default: value = null } = {}) => {
+            const output = {...o};
+            const rTo = render(to, 'RNDM.functionChain', { state });
+            const rFrom = render(from, 'RNDM.functionChain', { state });
+            const rDefault = render(value, 'RNDM.functionChain', { state });
+            set(output, rTo, get(state, rFrom, rDefault));
+            return output;
+        }, {})
 );
 
 export default buildState;
